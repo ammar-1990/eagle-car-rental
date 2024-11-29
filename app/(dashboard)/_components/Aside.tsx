0a9f70ad6afb876fb10ba30cd204dd2119d58277
ammar <ammar.ali.haidar.1990@gmail.com>
2024-11-29 23:04:49 +0400

    fix(eslint)

--- app/(dashboard)/_components/Aside.tsx
+++ app/(dashboard)/_components/Aside.tsx
@@ -1,26 +1,34 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
-import NavList from './NavList'
-import { SignoutButton } from './SignoutButton'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import NavList from "./NavList";
+import { SignoutButton } from "./SignoutButton";
 
-type Props = {}
+/* eslint-disable @typescript-eslint/no-unused-vars */
+// @typescript-eslint/no-empty-object-type
+type Props = {};
 
 const Aside = (props: Props) => {
   return (
-    <aside className='w-[240px] bg-site-primary p-[24px] flex flex-col gap-[8px]'>
-    {/* logo */}
-    <Link href={'/'}>
-    <div className='relative w-full aspect-video '>
-    <Image priority src={'/Logo.png'} className='object-contain' fill  alt='Logo' />
-    </div>
-    </Link>
-    {/* nav list */}
-    <NavList/>
-    {/* signout button */}
-    <SignoutButton />
+    <aside className="w-[240px] bg-site-primary p-[24px] flex flex-col gap-[8px]">
+      {/* logo */}
+      <Link href={"/"}>
+        <div className="relative w-full aspect-video ">
+          <Image
+            priority
+            src={"/Logo.png"}
+            className="object-contain"
+            fill
+            alt="Logo"
+          />
+        </div>
+      </Link>
+      {/* nav list */}
+      <NavList />
+      {/* signout button */}
+      <SignoutButton />
     </aside>
-  )
-}
+  );
+};
 
-export default Aside
+export default Aside;
